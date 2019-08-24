a9ff30f2e824cd4cc0b020e0ec5d9ec1ae69da6d
Henrik <hrc@prooffice.dk>
2019-08-24 08:59:36 +0200

    Tilføjede NH mapping

---
+++ FørsteNHibernateProjekt/Klasse1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FørsteNHibernateProjekt
+{
+    // En kommentar til at få Git til at reagere.
+    public class Klasse1
+    {
+        public string Navn { get; set; }
+        public string Kategori { get; set; }
+        public bool Udgået { get; set; }
+    }
+
+}
